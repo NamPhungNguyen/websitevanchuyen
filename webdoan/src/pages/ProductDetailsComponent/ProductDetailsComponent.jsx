b12df6ede440440bdcd80f9cb9c4956ed098efe8
NamPhungNguyen <namhien12082003@gmail.com>
2023-11-11 16:32:13 +0700

    lam tiep product chi tiet

--- webdoan/src/pages/ProductDetailsComponent/ProductDetailsComponent.jsx
+++ webdoan/src/pages/ProductDetailsComponent/ProductDetailsComponent.jsx
@@ -2,7 +2,8 @@ import { Col, Row, Image } from 'antd';
 import React from 'react';
 import imageProduct from '../../assets/images/anh_home.jpg'
 import imageContainer from '../../assets/images/anhcontainer.jpg'
-import { WrapperStyleColImage, WrapperStyleImageSmall } from './style';
+import { WrapperPriceAddressProduct, WrapperPriceProduct, WrapperPriceTextProduct, WrapperStyleColImage, WrapperStyleImageSmall, WrapperStyleNameProduct} from './style';
+import {StarFilled} from '@ant-design/icons'
 const ProductDetailsComponent = () => {
     return (
         <Row style={{padding:'16px'}}> 
@@ -23,7 +24,22 @@ const ProductDetailsComponent = () => {
                     </WrapperStyleColImage>
                 </Row>
             </Col>
-            <Col span={14}>col-12</Col>
+            <Col span={14}>
+                <WrapperStyleNameProduct>Hàng đông lạnh - Hoa quả Cao Bằng</WrapperStyleNameProduct>
+                <div>
+                    <StarFilled style={{fontSize: '12px', color: 'rgb(253, 216, 54)'}}/>
+                    <StarFilled style={{fontSize: '12px', color: 'rgb(253, 216, 54)'}}/>
+                    <StarFilled style={{fontSize: '12px', color: 'rgb(253, 216, 54)'}}/>
+                    <span> | Dang cho</span>
+                </div>
+                <WrapperPriceProduct>
+                    <WrapperPriceTextProduct>3.000.000  </WrapperPriceTextProduct>
+                </WrapperPriceProduct>
+                <WrapperPriceAddressProduct>
+                    <span>Giao đến</span>
+                    <span className='address'>Q.1, Thành phố Hồ Chí Minh</span>
+                </WrapperPriceAddressProduct>
+            </Col>
         </Row>
     );
 }
